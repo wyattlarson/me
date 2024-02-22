fb25acbd88249ac5bb87a86cea23a22e093e6214
Wyatt Larson <wyattlarson250@gmail.com>
2024-02-21 17:38:02 -0800

    Adding background video

--- src/Body/index.tsx
+++ src/Body/index.tsx
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import {
     Routes,
     Route,
-  } from "react-router-dom";
+} from "react-router-dom";
 import './index.css';
 import Home from './Home/home';
 import Walmart from './Walmart/walmart';
 
 
 function Body(props: any) {
-    let bodyStyle = {
-        'backgroundImage': `url("https://assets.website-files.com/5bac0565754dd6b3e278aeb2/5bac0565754dd6780a78af19_deckstop-background.svg"), linear-gradient(to right, ${props.activeColor}, ${props.activeColor})`
-    }
 
+    const videoRef = useRef<HTMLVideoElement>(null);
+
+    useEffect(() => {
+        if (videoRef.current) {
+            // Adjust the playback rate of the video
+            videoRef.current.playbackRate = 0.6; // Example: 0.5 for half speed
+        }
+    }, []);
     return (
-        <div className='body' style={bodyStyle}>
+        <div className='body'>
+            <video autoPlay muted loop className='background-video' ref={videoRef}>
+                <source src="https://firebasestorage.googleapis.com/v0/b/wyattlarsonblog.appspot.com/o/Untitled.mov?alt=media&token=0d1af663-8427-43c9-9685-564c2d7ac54f" type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
             <div className='window'>
                 <Routes>
                     <Route path="/" element={<Home activeColor={props.activeColor} />} />
